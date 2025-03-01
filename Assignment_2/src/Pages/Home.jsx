b9e9ecf8d2163cb3ef10e4a83916b39f53360976
Ashish <iashishyadav17@gmai.com>
2025-03-01 17:14:39 +0530

    Updated

---
+++ Assignment_2/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+            <h1>Ashish Yadav</h1>
+            <h2>Contact me: <a href="mailto:ashish.yadav.s84@kalvium.community">ashish.yadav.s84@kalvium.community</a></h2>
+    </div>
+  )
+}
